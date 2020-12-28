33544c69f976961fac04fe0e22c35e19a690fe1a
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-12-27 23:36:40 -0300

    Adding update and delete person methods

--- src/main/java/gabcamilo/demo/api/PersonController.java
+++ src/main/java/gabcamilo/demo/api/PersonController.java
@@ -7,9 +7,11 @@ import java.util.Optional;
 import java.util.UUID;
 import javax.swing.text.html.Option;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,4 +39,14 @@ public class PersonController {
   @GetMapping(path = "{id}")
   public Person getPersonById(@PathVariable("id") UUID id) { return personService.getPersonById(id)
       .orElse(null); }
+
+  @PutMapping(path = "{id}")
+  public void updatePersonById(@PathVariable("id")UUID id, @RequestBody Person newPerson) {
+    personService.updatePersonById(id, newPerson);
+  }
+
+  @DeleteMapping(path = "{id}")
+  public void deletePersonId(@PathVariable("id") UUID id) {
+    personService.deletePersonById(id);
+  }
 }
