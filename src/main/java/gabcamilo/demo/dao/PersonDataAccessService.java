d078a06d666475eedff79f63784d0738474b6c93
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2021-01-03 17:42:44 -0300

    Adding create, update and delete methods

--- src/main/java/gabcamilo/demo/dao/PersonDataAccessService.java
+++ src/main/java/gabcamilo/demo/dao/PersonDataAccessService.java
@@ -20,7 +20,16 @@ public class PersonDataAccessService implements PersonDao {
 
   @Override
   public int insertPerson(UUID id, Person person) {
-    return 0;
+    String sql = "INSERT INTO person (" +
+        "id," +
+        "name)" +
+        "VALUES (?, ?)";
+
+    return jdbcTemplate.update(
+        sql,
+        id,
+        person.getName()
+    );
   }
 
   @Override
@@ -50,11 +59,13 @@ public class PersonDataAccessService implements PersonDao {
 
   @Override
   public int deletePersonById(UUID id) {
-    return 0;
+    String sql = "DELETE FROM person WHERE id = ?";
+    return jdbcTemplate.update(sql, id);
   }
 
   @Override
   public int updatePersonById(UUID id, Person person) {
-    return 0;
+    String sql = "UPDATE person SET name = ? WHERE id = ?";
+    return jdbcTemplate.update(sql, person.getName(), id);
   }
 }
